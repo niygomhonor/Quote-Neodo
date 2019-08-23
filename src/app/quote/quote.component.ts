6c4e29c0bb3af8c348c330d59fcdbd4bacd81769
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-08-23 15:53:56 +0200

    Add class and fieldset tag

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -13,10 +13,7 @@ export class QuoteComponent implements OnInit {
     // {nbr:3, nameQuote:'Never give up',descriptQuote:'asdfghjk'}]
 
     new Quote(1,'You can make more friends in two months by becoming interested in other people than you can in two years by trying to get other people interested in you.','Dale Carnegie','I have to buy cookies for the parrot',new Date(2019,6,9)),
-    new Quote(2,'A friend is someone who understands your past, believes in your future, and accepts you just the way you are.','Unknown','Diana has her birthday coming up soon',new Date(2019,1,12)),
-    new Quote(3,'Never give up','Unknown','Pupper likes expensive snacks',new Date(2019,0,18)),
-    new Quote(4,'Never give up','Unknown','Damn Math',new Date(2019,2,14)),
-    new Quote(5,'Never give up','','Cause I am an evil overlord',new Date(2019,3,14)),];
+  ];
 
     toggleDetails(index){
       this.quotes[index].showDescription=! this.quotes[index].showDescription;
@@ -31,6 +28,12 @@ if (toDelete){
   this.quotes.splice(index,1)
 }
     }}
+    addNewQuote(quote){
+      let quoteLength=this.quotes.length;
+      quote.id=quoteLength+1;
+      quote.writtenDate=new Date(quote.writtenDate);
+      this.quotes.push(quote);
+    }
   constructor() { }
 
   ngOnInit() {
