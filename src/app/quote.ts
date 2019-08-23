efe86940e8221b72a3a57d0dd8a007606ed8c2f2
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-08-23 16:12:15 +0200

    Add constructor

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -4,7 +4,7 @@ export class Quote {
     // descriptQuote:string;
     // writtenDate: Date
     showDescription:boolean;
-    constructor( public  id:number,public nameQuote:string, public  descriptQuote:string, public writtenDate: Date) {
+    constructor( public  id:number,public  nameQuote:string, public quoteAuthor:string,public  descriptQuote:string, public writtenDate: Date) {
         this.showDescription=false;
     }
 }
