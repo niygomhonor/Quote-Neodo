64b8fd1e94422b4ebdc5a9d391eb96d34ce58824
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-08-25 12:06:25 +0200

    Add calculations for days

--- src/app/date-count.pipe.ts
+++ src/app/date-count.pipe.ts
@@ -1,24 +1,28 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import { Pipe, PipeTransform ,Input} from '@angular/core';
+import { Quote } from '@angular/compiler';
 
 @Pipe({
   name: 'dateCount'
 })
 export class DateCountPipe implements PipeTransform {
 
- 
-    transform(value: any): number {
+  @Input() quote:Quote;
+
+    transform(value:number, writtenDate:Date):number{
       let today:Date = new Date(); //get current date and time
       let todayWithNoTime:any = new Date(today.getFullYear(), today.getMonth(), today.getDate())
-      var dateSum = Math.abs(value -  todayWithNoTime) //returns value in miliseconds
+      var dateDifference = Math.abs(value - todayWithNoTime) //returns value in miliseconds
       const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
-      var dateSumSeconds = dateSum*0.001; //converts miliseconds to seconds
-      var dateCounter = dateSumSeconds/secondsInDay;
+      var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
+      var dateCounter = Math.round(dateDifferenceSeconds/secondsInDay) ;
   
-      if (dateCounter >= 1 && value > todayWithNoTime){
-        return dateCounter;}
-    else {
-      return 0;
+      if (dateCounter >= 1 ){
+        return dateCounter;
+      }else{
+        return 0;
+      }
     }
-  }
+  
+   }
 
-}
+
