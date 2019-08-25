741b3348bbef670b632ac45eb8bd4b43e93aaa18
NIYONKURU Honorine <niygomhonor@gmail.com>
2019-08-25 12:14:39 +0200

    Delete vote component

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,14 @@
+import { Directive, ElementRef, HostListener,Input} from '@angular/core';
+
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+
+  constructor( private elem:ElementRef) { 
+    this.elem.nativeElement.style.backgroundColor="blue";
+}
+
+}
